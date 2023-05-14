d7b324b45ee9ce2a4db8751783cfdb77684a673a
abraham-alizor <abraham.alizor@gmail.com>
2023-05-14 13:20:09 +0100

    route fixes

--- src/data/mainRouteData.ts
+++ src/data/mainRouteData.ts
@@ -21,50 +21,50 @@ export const mainRoutes = [
 		id: '2',
 		title: 'Balances',
 		component: Balances,
-		path: '/balance',
+		path: 'balance',
 	},
 	{
 		id: '3',
 		title: 'Transactions',
 		component: Transactions,
-		path: '/transaction',
+		path: 'transaction',
 	},
 	{
 		id: '4',
 		title: 'Analytics',
 		component: Analytics,
-		path: '/announcement',
+		path: 'announcement',
 	},
 	{
 		id: '5',
 		title: 'Marketing',
 		component: Marketing,
-		path: '/marketing',
+		path: 'marketing',
 	},
 	{
 		id: '6',
 		title: 'Exchange rates',
 		component: Exchange,
-		path: '/exchange-rates',
+		path: 'exchange-rates',
 	},
 
 	{
 		id: '7',
 		title: 'Checkout',
 		component: Checkout,
-		path: '/checkout',
+		path: 'checkout',
 	},
 	{
 		id: '8',
 		title: 'Payment links',
 		component: PaymentLinks,
-		path: '/payment-links',
+		path: 'payment-links',
 	},
 
 	{
 		id: '9',
 		title: 'Wire',
 		component: Wire,
-		path: '/wire',
+		path: 'wire',
 	},
 ];
